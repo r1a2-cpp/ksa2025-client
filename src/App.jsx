7e8b14936c964b03c75b3a009a7805b7b829d638
riaz <riajul.swe@gmail.com>
2025-10-29 14:44:35 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,24 @@ function App() {
               <th>Action</th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <th>Riajul Islam</th>
+              <th>A123456</th>
+              <th>10/29/2025</th>
+              <th>Rahman</th>
+              <th>FIT</th>
+              <th>OK</th>
+              <th>OK</th>
+              <th>OK</th>
+              <th>OK</th>
+              <th>OK</th>
+              <th>OK</th>
+              <th>COMPLEATE</th>
+              <th>❌</th>
+            </tr>
+          </tbody>
         </table>
       </div>
     </>
