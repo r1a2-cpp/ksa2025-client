c3a8e20cb299ea01e541c8747401fa00e06cb6c4
riaz <riajul.swe@gmail.com>
2025-10-29 17:28:16 +0600

    fronend init

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div className="container">
         <div className="header-row">
           <h2>Candidate Dashborad</h2>
           <form className="add-form">
             <input 
-              type="number"
+              type="text"
               name="sl"
               placeholder="SL"
             />
@@ -33,34 +32,33 @@ function App() {
               placeholder="Agent"
             />
             <button>Add Candidate</button>
+
           </form>
         </div>
-
-        <div className="summary-cards full-witdh">
+        <div className="summary-cards full-width">
           <div className="card">Total<span>10</span></div>
           <div className="card">Medical<span>10</span></div>
-          <div className="card">Mofa<span>10</span></div>
           <div className="card">Visa<span>10</span></div>
           <div className="card">Manpower<span>10</span></div>
           <div className="card">Flight<span>10</span></div>
-          <div className="card">Complete<span>10</span></div>
-          <div className="card">Cancel<span>10</span></div>
+          <div className="card">Compleate<span>10</span></div>
+          <div className="card cancel">Cancel<span>10</span></div>
         </div>
-        <div className='sort-section'>
+        <div className="sort-section">
           <label>Sort by</label>
           <select>
             <option>Newest First</option>
             <option>Oldest First</option>
           </select>
         </div>
-
+        <div className="message-box"> Message Here</div>
         <table>
           <thead>
             <tr>
-              <th>Sl</th>
+              <th>SL</th>
               <th>Name</th>
               <th>Passport</th>
-              <th>Recived</th>
+              <th>Received</th>
               <th>Agent</th>
               <th>Med</th>
               <th>Mofa</th>
@@ -76,10 +74,10 @@ function App() {
           <tbody>
             <tr>
               <td>1</td>
-              <td>Riajul Islam</td>
+              <td>RIAJUL ISLAM</td>
               <td>A123456</td>
-              <td>10/29/2025</td>
-              <td>Rahman</td>
+              <td>10/19/2025</td>
+              <td>RAHMAN</td>
               <td>FIT</td>
               <td>OK</td>
               <td>OK</td>
@@ -88,14 +86,14 @@ function App() {
               <td>OK</td>
               <td>OK</td>
               <td>COMPLEATE</td>
-              <td><button>
-                  ✖
-                </button></td>
+              <td>
+                <button className="edit-btn">✎</button>
+              </td>
             </tr>
           </tbody>
         </table>
+
       </div>
-    </>
   )
 }
 
